1a5af05d27ce59e9cacf3182eb3474e2e4f45513
maidzui123 <maiduy190802@gmail.com>
2025-02-17 09:44:42 +0700

    add forget password UI

--- client/src/constants/types/auth.ts
+++ client/src/constants/types/auth.ts
@@ -24,4 +24,14 @@ export type signUpDef = signInDef & {
     confirmPassword: string;
 }
 
+export type forgetPasswordDef = {
+    email: string;
+}
+
+
+export type resetPasswordDef = {
+    password: string;
+    confirmPassword: string;
+}
+
 export type userDataDef = userInfoDef & { avatar: string, _id: string, accessToken: string, isOnline: boolean, mutualFriends: number, username: string, dob: string, tel: string, bio: string }
